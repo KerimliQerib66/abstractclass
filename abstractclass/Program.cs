00ef00125c1b417d3820f4a58f49287834a8478e
user <user@WINDOWS-DRRTF68>
2022-07-27 14:12:41 +0400

    Add project files.

---
+++ abstractclass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractclass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+   abstract class myclass
+    {
+        abstract public void x();
+        abstract public int REQEM { get; set; }
+    }
+
+    class numune : myclass
+    {
+        public override int REQEM { get ; set ; }
+
+        public override void x()
+        {
+            Console.WriteLine("salam");
+        }
+    }
+}
